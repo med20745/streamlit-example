59b50442422818fcbf918c20ee5c1d6300ec8e4c
med20745 <128611207+med20745@users.noreply.github.com>
2023-06-30 16:15:59 +0100

    Update interface.py

--- interface.py
+++ interface.py
@@ -19,6 +19,34 @@ else:
     cursor=None
     df=None
     nombre_ligne = 1
+
+st.write('Linses Classification by Passes')
+result_df=pd.DataFrame()
+for i in [1,2,3]:
+    query=f'SELECT COUNT(*)FROM a WHERE QTE={i}'
+    cursor.execute(query)
+    resultat=cursor.fetchall()
+    
+    if i==1:
+        column_name1 = f"{i}er pass"
+        column_name='pourcentage'+f"{i}er pass"
+        
+    else:
+        column_name1 = f"{i}ème pass"
+        column_name = 'pourcentage' + f"{i}ème pass"
+        
+temp_df = pd.DataFrame(resultat, columns=[column_name1])  # temporary data frame
+temp_df1 = pd.DataFrame([[resultat[0][0] / nombre_ligne * 100]], columns=[column_name])#data frame pourcentage
+temp_df = pd.concat([temp_df, temp_df1], axis=1)
+result_df = pd.concat([result_df, temp_df], axis=1)
+bar_chart = px.bar(result_df, title='Pass Distribution (number)', x=['1st pass ','2nd pass','3rd pass'], y=[result_df.loc[0][0],result_df.loc[0][2],result_df.loc[0][4]])
+bar_chart.update_layout(
+    xaxis_title='Pass Number',  # Nom de l'axe des abscisses
+    yaxis_title='Quantity'
+)
+pie_char=px.pie(result_df,title='Distribution of  Total Quantity',values=[result_df.loc[0][1],result_df.loc[0][3],result_df.loc[0][5]],names=['pourcentage 1 pass' ,'pourcentage 2 pass ','pourcentage 3 pass'])
+st.plotly_chart(bar_chart)
+st.plotly_chart(pie_char)
     
 result_df=pd.DataFrame()#empty data frame
 
